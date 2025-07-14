9585c0eab14836e44ced724ba71a5788354ae00a
yashjuneja <yash.juneja@gmail.com>
2025-07-14 20:22:37 +0100

    Added Get and Delete functionality for user

--- UserGroupManagement.Client/Services/IUserApiService.cs
+++ UserGroupManagement.Client/Services/IUserApiService.cs
@@ -5,5 +5,9 @@ namespace UserGroupManagement.Client.Services
     public interface IUserApiService
     {
         Task<List<UserDto>> GetAllUsersAsync();
+        Task<UserDto> GetUserByIdAsync(int id);
+        Task<UserDto> CreateUserAsync(UserDto user);
+        Task<UserDto> UpdateUserAsync(UserDto user);
+        Task<UserDto> DeleteUserAsync(int id);
     }
 }
--- UserGroupManagement.Client/Services/UserApiService.cs
+++ UserGroupManagement.Client/Services/UserApiService.cs
@@ -11,10 +11,35 @@ namespace UserGroupManagement.Client.Services
             _httpClient = httpClient;
         }
 
+        public async Task<UserDto> CreateUserAsync(UserDto user)
+        {
+            var response = await _httpClient.PostAsJsonAsync("api/Users/CreateUser", user);
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadFromJsonAsync<UserDto>();
+        }
+
+        public async Task<UserDto> DeleteUserAsync(int id)
+        {
+            var response = await _httpClient.DeleteAsync($"api/Users/DeleteUser/{id}");
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadFromJsonAsync<UserDto>();
+        }
+
         public async Task<List<UserDto>> GetAllUsersAsync()
         {
             var result = await _httpClient.GetFromJsonAsync<List<UserDto>>("api/Users/GetUsers");
             return result ?? new List<UserDto>();
+        }
+
+        public async Task<UserDto> GetUserByIdAsync(int id) =>
+            await _httpClient.GetFromJsonAsync<UserDto>($"api/Users/GetUserbyId/{id}") ?? new UserDto();
+        
+
+        public async Task<UserDto> UpdateUserAsync(UserDto user)
+        {
+            var response = await _httpClient.PostAsJsonAsync($"api/Users/UpdateUser/{user.Id}", user);
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadFromJsonAsync<UserDto>();
         }
     }
 }
